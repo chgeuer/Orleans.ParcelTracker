70f86f3f62842d6233bf1c6bbfd7ff91be10ef4e
Yoav Dobrin <yodobrin@microsoft.com>
2023-03-08 10:25:52 +0200

    align

--- ParcelTracker.Host/Program.cs
+++ ParcelTracker.Host/Program.cs
@@ -4,31 +4,42 @@ using Azure.Identity;
 using Microsoft.Extensions.Hosting;
 using Orleans.Hosting;
 using System;
+using Azure.Core;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 
 
+
 internal class Program
 {
+    private static string? env(HostBuilderContext context,string name) => context.Configuration.GetValue<string>(name);
+
+    static TokenCredential GetStorageCredential(HostBuilderContext context)
+        => new ClientSecretCredential(
+            tenantId: env(context, "ORLEANS_TENANT_ID"),
+            clientId: env(context,"ORLEANS_GRAIN_STORAGE_CLIENT_ID"),
+            clientSecret: env(context,"ORLEANS_GRAIN_STORAGE_CLIENT_SECRET"));
+
+    static (string ServiceUrl, string ContainerName) GetStorage(HostBuilderContext context)
+        => (env(context,"SERVICE_URI")?? string.Empty, "grainstate");
+
 
     static async Task Main(string[] args)
     {
         Console.Title = "Host";
        
+        // var storage = GetStorage();
+
         using var host = Host
             .CreateDefaultBuilder(args)
-            .ConfigureAppConfiguration(ConfigureApp)            
-            .UseOrleans( (context, sb) => sb               
+            .ConfigureAppConfiguration(ConfigureApp)     
+            .UseOrleans((context, sb) => sb
                 .UseLocalhostClustering()
+                .UseInMemoryReminderService()
                 .AddAzureBlobGrainStorage("blobGrainStorage", o =>
                 {
-                    o.ConfigureBlobServiceClient(
-                        serviceUri: new(context.Configuration.GetValue<string>("SERVICE_URI") ?? string.Empty),
-                        tokenCredential: new ClientSecretCredential(
-                            tenantId: context.Configuration.GetValue<string>("ORLEANS_TENANT_ID"),
-                            clientId: context.Configuration.GetValue<string>("ORLEANS_GRAIN_STORAGE_CLIENT_ID"),
-                            clientSecret: context.Configuration.GetValue<string>("ORLEANS_GRAIN_STORAGE_CLIENT_SECRET")));
-                    o.ContainerName = "grainstate";
+                    o.ConfigureBlobServiceClient(serviceUri: new(GetStorage(context).ServiceUrl), tokenCredential: GetStorageCredential(context));
+                    o.ContainerName = GetStorage(context).ContainerName;
                 })
             )
             .Build();
@@ -43,7 +54,7 @@ internal class Program
     private static void ConfigureApp(HostBuilderContext hostBuilderContext, IConfigurationBuilder configurationBuilder)
     {
         // try and load env params from appsettings.json
-        configurationBuilder.AddJsonFile("appsettings.json");
+        configurationBuilder.AddJsonFile("appsettings_local.json");
         CheckConfiguration(configurationBuilder.Build());
     }
 
