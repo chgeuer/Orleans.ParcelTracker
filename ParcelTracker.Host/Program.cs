dc688a5977a7593f6bcb19cee9f305aa3af01c0a
Yoav Dobrin <yodobrin@microsoft.com>
2023-03-08 09:57:03 +0200

    using appsetting for env param

--- ParcelTracker.Host/Program.cs
+++ ParcelTracker.Host/Program.cs
@@ -5,25 +5,29 @@ using Microsoft.Extensions.Hosting;
 using Orleans.Hosting;
 using System;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
 
 internal class Program
 {
+
     static async Task Main(string[] args)
     {
         Console.Title = "Host";
-
+       
         using var host = Host
             .CreateDefaultBuilder(args)
-            .UseOrleans(sb => sb
+            .ConfigureAppConfiguration(ConfigureApp)            
+            .UseOrleans( (context, sb) => sb               
                 .UseLocalhostClustering()
                 .AddAzureBlobGrainStorage("blobGrainStorage", o =>
                 {
                     o.ConfigureBlobServiceClient(
-                        serviceUri: new("https://chgpes1.blob.core.windows.net/"),
+                        serviceUri: new(context.Configuration.GetValue<string>("SERVICE_URI") ?? string.Empty),
                         tokenCredential: new ClientSecretCredential(
-                            tenantId: Environment.GetEnvironmentVariable("ORLEANS_TENANT_ID"),
-                            clientId: Environment.GetEnvironmentVariable("ORLEANS_GRAIN_STORAGE_CLIENT_ID"),
-                            clientSecret: Environment.GetEnvironmentVariable("ORLEANS_GRAIN_STORAGE_CLIENT_SECRET")));
+                            tenantId: context.Configuration.GetValue<string>("ORLEANS_TENANT_ID"),
+                            clientId: context.Configuration.GetValue<string>("ORLEANS_GRAIN_STORAGE_CLIENT_ID"),
+                            clientSecret: context.Configuration.GetValue<string>("ORLEANS_GRAIN_STORAGE_CLIENT_SECRET")));
                     o.ContainerName = "grainstate";
                 })
             )
@@ -35,4 +39,24 @@ internal class Program
         _ = await Console.In.ReadLineAsync();
         await host.StopAsync();
     }
+
+    private static void ConfigureApp(HostBuilderContext hostBuilderContext, IConfigurationBuilder configurationBuilder)
+    {
+        // try and load env params from appsettings.json
+        configurationBuilder.AddJsonFile("appsettings.json");
+        CheckConfiguration(configurationBuilder.Build());
+    }
+
+    private static void CheckConfiguration(IConfiguration configuration)
+    {
+        // confirm all environment variables are set
+        if( string.IsNullOrEmpty(configuration.GetValue<string>("ORLEANS_TENANT_ID")) ||
+            string.IsNullOrEmpty(configuration.GetValue<string>("ORLEANS_GRAIN_STORAGE_CLIENT_ID")) ||
+            string.IsNullOrEmpty(configuration.GetValue<string>("ORLEANS_GRAIN_STORAGE_CLIENT_SECRET")) ||
+            string.IsNullOrEmpty(configuration.GetValue<string>("SERVICE_URI")))
+            {
+                throw new ArgumentNullException("Missing Mandatory Environment Variables");        
+            }                   
+    }
+           
 }
